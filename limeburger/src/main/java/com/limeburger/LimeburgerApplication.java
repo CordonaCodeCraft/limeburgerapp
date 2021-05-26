4d5afefaf83bf6eb255e5b637800dc89b1efead2
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-05-26 17:02:22 +0300

    Initial commit

---
+++ limeburger/src/main/java/com/limeburger/LimeburgerApplication.java
@@ -0,0 +1,13 @@
+package com.limeburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeburgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeburgerApplication.class, args);
+    }
+
+}
---
+++ limeburger/src/test/java/com/limeburger/LimeburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limeburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeburgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
