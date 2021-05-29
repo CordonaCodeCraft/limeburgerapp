97bbc16923ad572d4f162ff360fb87bc5fa9b001
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-05-29 12:09:33 +0300

    Missed commit of essential changes. Addresses #16

--- limeburger/src/main/java/com/limeburger/domain/burger/mapper/BurgerMapper.java
+++ limeburger/src/main/java/com/limeburger/domain/burger/mapper/BurgerMapper.java
@@ -3,18 +3,21 @@ package com.limeburger.domain.burger.mapper;
 import com.limeburger.domain.allergen.dto.AllergenCustomerView;
 import com.limeburger.domain.allergen.mapper.AllergenMapper;
 import com.limeburger.domain.burger.dto.admin.BurgerAdminView;
+import com.limeburger.domain.burger.dto.customer.BurgerCustomerCommand;
+import com.limeburger.domain.burger.dto.customer.BurgerCustomerComposed;
 import com.limeburger.domain.burger.dto.customer.BurgerCustomerView;
 import com.limeburger.domain.burger.model.Burger;
 import com.limeburger.domain.ingredient.dto.IngredientAdminView;
 import com.limeburger.domain.ingredient.dto.IngredientCustomerView;
 import com.limeburger.domain.ingredient.mapper.IngredientMapper;
 import com.limeburger.domain.ingredient.model.Ingredient;
-import com.limeburger.domain.util.MapperService;
+import com.limeburger.domain.ingredient.repository.IngredientsLookupTable;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -38,7 +41,7 @@ public interface BurgerMapper extends MapperService {
       source = "isInPromotion",
       target = "isInPromotion",
       qualifiedByName = "promotionBooleanToPromotionString")
-  BurgerCustomerView toBurgerCustomerView(Burger burger);
+  BurgerCustomerView toBurgerCustomerView(final Burger source);
 
   @Mapping(source = "burgerType", target = "type", qualifiedByName = "BurgerEnumToBurgerEnumValue")
   @Mapping(
@@ -53,7 +56,13 @@ public interface BurgerMapper extends MapperService {
       source = "productionCost",
       target = "productionCost",
       qualifiedByName = "decimalToString")
-  BurgerAdminView toBurgerAdminView(final Burger burger);
+  BurgerAdminView toBurgerAdminView(final Burger source);
+
+  @Mapping(
+      source = "ingredients",
+      target = "ingredients",
+      qualifiedByName = "ingredientsListToIngredientSet")
+  BurgerCustomerComposed toBurgerCustomerComposed(final BurgerCustomerCommand source);
 
   @Named("BurgerEnumToBurgerEnumValue")
   static String convertBurgerEnumToBurgerStringValue(final BurgerType target) {
@@ -85,6 +94,15 @@ public interface BurgerMapper extends MapperService {
   static Set<IngredientAdminView> convertIngredientsToAdminView(final Set<Ingredient> target) {
     return target.stream()
         .map(IngredientMapper.INSTANCE::toIngredientAdminView)
+        .collect(Collectors.toSet());
+  }
+
+  @Named("ingredientsListToIngredientSet")
+  static Set<IngredientCustomerView> convertIngredientsListToIngredientSet(
+      final List<String> target) {
+    return target.stream()
+        .map(i -> IngredientsLookupTable.getTable().get(i))
+        .map(IngredientMapper.INSTANCE::toIngredientCustomerView)
         .collect(Collectors.toSet());
   }
 }
