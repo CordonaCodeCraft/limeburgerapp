46e621a8f1703343cb0d36499217c64e1a282c1a
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-05-29 20:38:13 +0300

    Implemented rate limits with bucket4j and other related dependencies. Closes #13

--- limeburger/src/main/java/com/limeburger/LimeBurgerApplication.java
+++ limeburger/src/main/java/com/limeburger/LimeBurgerApplication.java
@@ -2,8 +2,10 @@ package com.limeburger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LimeBurgerApplication {
 
   public static void main(String[] args) {
